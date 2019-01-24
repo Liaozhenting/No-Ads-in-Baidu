a565487ca8a76d713bf55d3b47346970c4122180
LiaoZhenting <1782708098@qq.com>
2019-01-24 23:47:06 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # No-Ads-in-Baidu
-去掉百度搜索结果里的广告
+谷歌浏览器去掉百度搜索结果里的广告
+
+1. 首先要在谷歌应用商用商店安装浏览器插件 tampermonkey。
+[地址](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?utm_source=chrome-ntp-icon)
+
+2. 然后添加新脚本
+
+![](./捕获.PNG)
+
+3. 然后复制本项目的代码进去，ctrl+S保存
+
+![](./捕获2.PNG)
+
+4. 进入百度看效果
+没有脚本之前
+
+![](./运行前.PNG)
+
+运行脚本后
+
+![](./运行后.PNG)
+
---
+++ index.js
@@ -0,0 +1,32 @@
+
+// ==UserScript==
+// @name         No Ads In Baidu
+// @namespace    https://www.baidu.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.baidu.com/*
+// @grant        none
+// ==/UserScript==
+
+function noAds() {
+    // 占位大，靠前的广告
+    $('[style="display:block !important;visibility:visible !important"]').hide();
+    // 搜索结果里的广告
+    setTimeout(function(){
+        $('.result.c-container').has('.f13 .m:contains("广告")').hide();
+    },2000)
+
+}
+
+$(document).ajaxComplete(function (event, xhr, settings) {
+    console.log('settings', settings);
+    // 搜索结果分页
+    if (/\/s\?ie=utf-8&mod=1&isbd=1&isid=/.test(settings.url)) {
+        noAds();
+    }
+});
+
+$(function () {
+    noAds();
+})
